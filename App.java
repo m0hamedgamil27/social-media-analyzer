5387c5c564da5866965d891bf886a484229a1e06
m0hamedgamil27 <mohamedgamil2722005@gmail.com>
2026-01-21 04:43:32 +0200

    Refactor App to use empty strings for access token and user ID
    Removed hardcoded access token and user ID.

--- App.java
+++ App.java
@@ -3,8 +3,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        String accessToken = "EAAIXaqZA8lDIBOZBZClCK0Umrc7rsFAQq9w7ZCfZAhII2ZADV6ZCaFb05yLRZC9bzNLlbrODewc3eC76DbJMxWWNACOg5rf6kaHBAFsEEExHqemEau27AygRlnQ0vkqVSl1ZCVG8TEAZByFCglEeUTyx3BMhZBU7NQ6FJ2qwiQVPxNiwxckRjHOTuuUr9hdGJHVWOjb2UGC8ZBsett9rnPuN1dIRbIzILZA4BKKDDp4YOmTz4CU7357vUw6v0";
-        String userId = "1602960493917937";
+        String accessToken = "";
+        String userId = "";
 
         SocialMediaPlatform facebook = new FacebookPlatform(accessToken);
 
@@ -16,3 +16,4 @@ public class App
         
     }
 }
+
